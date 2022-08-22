ea5bcad43a59f1b10a3a4b6b7ce78e491e6da63a
Sumin <sumin.kim.dev@gmail.com>
2022-08-22 13:21:49 +0900

    220822

---
+++ baekjoon/브루트 포스/1436.py
@@ -0,0 +1,12 @@
+# **참고**
+
+n = int(input())
+fin = 666
+total = 0
+while True:
+    if('666' in str(fin)):
+        total += 1
+    if(total == n):
+        print(fin)
+        break
+    fin += 1
